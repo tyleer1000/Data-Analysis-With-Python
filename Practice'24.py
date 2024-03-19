a5de62eb276fb2e80cda690fc64315c9140deaf1
tyleer1000 <63994625+tyleer1000@users.noreply.github.com>
2024-03-18 22:46:06 -0700

    practice

--- Practice'24.py
+++ Practice'24.py
@@ -1,21 +1,62 @@
 import pandas as pd
 import numpy as np
 
+data = pd.read_csv("2.1 2.csv")
+
+data = data.dropna()
+
+#print(data.head(5))
 
 def practice():
-    data = pd.read_csv("Student_Marks.csv")
 
     print(data.head(5))
 
-    print(data.isnull().sum())
+    #print(data.isnull.sum())
 
-    #print(data.describe())
-    #print(data['Marks'])
+    #What Origin has the most number of cars? 
 
-    data_table = data[['Marks']]
+    #print(data['Origin'].value_counts())
+    print(data['Origin'].describe())
+#practice()
 
-    print(data_table)
-    print(data_table.head(4))
-practice()
+"""
+Analyze cars in the Origin of Asia
+"""
+def only_asia():
+
+    data_asia = data[data['Origin']=='Asia']
+
+    #print(data[data['Origin']=='Asia'].describe())
+
+    print(data_asia['Origin'].value_counts())
+    
+#only_asia()
+    
+"""
+Cars with Engsize >2.3
+"""
+def big_cylinders():
+    data_eng = data[data['EngineSize'] >2.3]
+    print(data_eng.value_counts())
+    print(data_eng['EngineSize'].min())
+    print(data_eng.describe())
+
+#big_cylinders()
+
+#print(data.iloc[:9,-9:])
+#print(data.loc[:,['Make', 'Origin']])
 
 #Two types of data structures in Pandas. Series and Table. 
+#to get rid of null values, could use dropna, notnull. 
+    
+'''Remove commas from invoice and convert to a float so you can perform operations on it. '''
+
+def clean_invoice():
+
+    data['Invoice'] = data['Invoice'].str.replace(",", "")
+    data['Invoice'] = data['Invoice'].str.replace("$", "")
+    data['Invoice'] = data['Invoice'].astype(float)
+    print(data.dtypes)
+    print(data.head(5))
+
+clean_invoice()
